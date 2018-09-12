de48433ffc6d4c72fbb400378b9efd61884a7fb2
Mike Shea <emtshea@gmail.com>
2018-09-12 08:53:52 -0400

    add loadFeed test

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -82,6 +82,7 @@ $(function() {
     });
 
     /* TODO: Write a new test suite named "Initial Entries" */
+    describe('Initial Entries', function(){
 
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
@@ -89,6 +90,15 @@ $(function() {
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
+        //using beforeEach to make sure there is at least one entry when loadFeed function completes
+        beforeEach(function(done){
+            loadFeed(0);
+        });
+        if('entry is present when loadFeed is completed', function(){
+            var checkEntry = $('.feed .entry').length;
+            expect(checkEntry).toBeGreaterThan(0);
+        });
+    });
 
     /* TODO: Write a new test suite named "New Feed Selection" */
 
